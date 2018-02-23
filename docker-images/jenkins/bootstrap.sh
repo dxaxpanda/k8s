3a06a19e36b37265d6cb02c0c2eda6930e8ad979
Jérémy MIRRE <jeremy.mirre@skores.com>
2018-02-23 11:23:38 +0100

    normalize bootstrap script

--- docker-images/jenkins/bootstrap.sh
+++ docker-images/jenkins/bootstrap.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-docker volume create jenkins || echo "Volume already created."
+printf "[*]\t%s\n" "Creating jenkins data volume."
+docker volume create jenkins 
 docker rm -f jenkins-master || echo "No jenkins container running."
-echo "[*] Starting new container"
+printf "[*]\t%s\n"  "Starting new container"
 docker run -d --name=jenkins-master -p 8080:8080 -p 50000:50000 -v jenkins:/var/jenkins_home skores/jenkins-master:lts-alpine
