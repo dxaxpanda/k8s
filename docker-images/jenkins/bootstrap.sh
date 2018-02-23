1c75848519bdbe5d0a7c3ba3a59ba3f232b0b21f
Jérémy MIRRE <jeremy.mirre@skores.com>
2018-02-23 10:18:39 +0100

    modified key files

--- docker-images/jenkins/bootstrap.sh
+++ docker-images/jenkins/bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker volume create jenkins
+docker volume create jenkins || echo "Volume already created."
 docker rm -f jenkins-master || echo "No jenkins container running."
 echo "[*] Starting new container"
 docker run -d --name=jenkins-master -p 8080:8080 -p 50000:50000 -v jenkins:/var/jenkins_home skores/jenkins-master:lts-alpine
