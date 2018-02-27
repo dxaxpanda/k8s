a1e80a2450cae35967f120ca6f89973a1865bbac
dxaxpanda <jmmirre@gmail.com>
2018-02-27 11:34:00 +0100

    added restartpolicy

--- docker-images/jenkins/bootstrap.sh
+++ docker-images/jenkins/bootstrap.sh
@@ -4,4 +4,4 @@ printf "[*]\t%s\n" "Creating jenkins data volume."
 docker volume create jenkins 
 docker stop jenkins-master && docker rm -f jenkins-master || echo "No container found."
 printf "[*]\t%s\n"  "Starting new container"
-docker run -d --name=jenkins-master -p 127.0.0.1:8080:8080 -p 10.0.0.22:50000:50000 -v jenkins:/var/jenkins_home skores/jenkins-master:lts-alpine
+docker run -d --restart=always --name=jenkins-master -p 127.0.0.1:8080:8080 -p 10.0.0.22:50000:50000 -v jenkins:/var/jenkins_home skores/jenkins-master:lts-alpine
