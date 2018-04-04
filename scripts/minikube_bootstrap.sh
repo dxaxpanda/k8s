9aea44a83b612f682d360c718508f4ddd06a63bb
dxaxpanda <jmmirre@gmail.com>
2018-04-04 12:16:49 +0200

    minikube install added

--- scripts/minikube_bootstrap.sh
+++ scripts/minikube_bootstrap.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.25.2/minikube-linux-amd64 && \
+	chmod +x minikube && \ 
+	sudo mv minikube /usr/local/bin/
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl && \
 	 chmod +x ./kubectl && \
 	  sudo mv ./kubectl /usr/local/bin/kubectl
